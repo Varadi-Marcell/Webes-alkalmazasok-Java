d13c1d20ae6f8403b9ddaeb8e1f2dfb2198e0a97
Varadi-Marcell <marcibazsi11@gmail.com>
2021-10-07 23:20:00 +0200

    konyv entitas

---
+++ src/main/java/uni/miskolc/book_entity/BookEntityApplication.java
@@ -0,0 +1,13 @@
+package uni.miskolc.book_entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/miskolc/book_entity/BookEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.miskolc.book_entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
