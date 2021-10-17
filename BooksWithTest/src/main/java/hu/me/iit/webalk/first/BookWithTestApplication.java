455efcfad7e2bdd6919cde26a4d03df9f94b0462
Varadi-Marcell <78903391+Varadi-Marcell@users.noreply.github.com>
2021-10-17 14:48:26 +0200

    Add files via upload

---
+++ BooksWithTest/src/main/java/hu/me/iit/webalk/first/BookWithTestApplication.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWithTestApplication.class, args);
+	}
+
+}
